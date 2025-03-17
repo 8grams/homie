a5514cf2e643a0e9ee32ac7d5b4685d7708f6697
Audi Nugraha <audinue@gmail.com>
2025-03-17 15:52:24 +0700

    Serve assets

--- src/libs/Controller.php
+++ src/libs/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Libs;
 
 use App\Libs\Interfaces\BaseController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +12,11 @@ class Controller extends BaseController
 {
     public function render(Request $request): Response
     {
+        if (strpos($request->getPathInfo(), '/data/assets/') === 0) {
+            $response = new BinaryFileResponse('..' /* getcwd() === './public' */ . $request->getPathInfo());
+            $response->headers->set('Content-Type', $response->getFile()->getMimeType());
+            return $response;
+        }
         return $this->renderPage($request);
     }
 
@@ -27,9 +33,9 @@ class Controller extends BaseController
 
         $view = $this->viewEngine->make($path);
         $view->setDependencies(
-            $this->request, 
-            $this->cache, 
-            $this->db, 
+            $this->request,
+            $this->cache,
+            $this->db,
             $this->blog,
             $this->config
         );
