748847f7b853ec0483da97905dc88ab45a85393d
Glend Maatita <glend.maatita@gmail.com>
2025-04-08 11:51:41 +0700

    initial writer class

--- src/libs/Writer.php
+++ src/libs/Writer.php
@@ -90,10 +90,30 @@ class Writer implements BlogInterface
             }
         }
 
-        $response = $this->client->request('GET', 'posts', ['query' => $useOptions]);
+        $pdo = $this->db->getPdo();
+
+        if ($useOptions['categories']) {
+            $response = $pdo->query("SELECT * FROM blogs WHERE language = '{$this->defaultLang}' 
+            AND category_id IN (".implode(",", $useOptions['categories']).") 
+            LIMIT {$useOptions['per_page']} OFFSET {$useOptions['offset']}")->fetchAll();
+        } else if ($useOptions['tags']) {
+            $tags = $pdo->query(
+                "SELECT * FROM tags_blogs WHERE tag_id IN (".implode(",", $useOptions['tags']).")"
+            )->fetchAll();
+            
+            $blogIds = array_map(function($tag) { 
+                return $tag['blog_id']; }, $tags
+            );
+            
+            $response = $pdo->query(
+                "SELECT * FROM blogs WHERE language = '{$this->defaultLang}' AND id IN (".implode(",", $blogIds).") LIMIT {$useOptions['per_page']} OFFSET {$useOptions['offset']}"
+            )->fetchAll();
+        } else {
+            $response = $pdo->query("SELECT * FROM blogs WHERE language = '{$this->defaultLang}' LIMIT {$useOptions['per_page']} OFFSET {$useOptions['offset']}")->fetchAll();
+        }
 
         // construct blog post
-        foreach ($response->toArray() as $post) {
+        foreach ($response as $post) {
             $posts[] = $this->constructPost($post);
         }
 
@@ -120,8 +140,9 @@ class Writer implements BlogInterface
             }
         }
 
-        $response = $this->client->request('GET', 'posts/' . $id, ['query' => ['_embed' => true]]);
-        $post = $response->toArray();
+        $pdo = $this->db->getPdo();
+        $response = $pdo->query("SELECT * FROM blogs WHERE id = {$id}")->fetch();
+        $post = $response->fetch();
         
         if ($this->cacheEnabled) {
             $this->cache->set($cacheKey, $post, $this->cacheAge);
@@ -137,35 +158,32 @@ class Writer implements BlogInterface
      */
     private function constructPost($post)
     {
+        // get author
         $author = new Author(
-            $post['_embedded']['author'][0]['id'],
-            $post['_embedded']['author'][0]['name'],
-            $post['_embedded']['author'][0]['avatar_urls']['96']
+            "1",
+            "Admin",
+            "https://raw.githubusercontent.com/8grams/homie/refs/heads/develop/assets/logo.png",
         );
 
+        // get categories
         $categories = [];
-        $blogc = $post['_embedded']['wp:term'][0];
-        foreach ($blogc as $category) {
-            $categories[] = new Category($category['id'], $category['name'], $category['slug']);
-        }
+        $categories = $pdo->query("SELECT * FROM categories WHERE id = {$post['category_id']}")->fetchAll();
 
+        // get tags
         $tags = [];
-        $blogt = $post['_embedded']['wp:term'][1];
-        foreach ($blogt as $tag) {
-            $tags[] = new Tag($tag['id'], $tag['name'], $tag['slug']);
-        }
+        $tags = $pdo->query("SELECT * FROM tags_blogs WHERE blog_id = {$post['id']}")->fetchAll();
 
         return new Post(
             $post['id'],
-            $post['title']['rendered'],
+            $post['title'],
             $categories,
             $tags,
-            $post['excerpt']['rendered'],
-            $post['content']['rendered'],
+            $post['excerpt'],
+            $post['content'],
             $author,
             date("d M Y", strtotime($post['date'])),
-            $post['link'],
-            isset($post['_embedded']['wp:featuredmedia']) ? $post['_embedded']['wp:featuredmedia'][0]['source_url'] : "https://raw.githubusercontent.com/8grams/homie/refs/heads/develop/assets/logo.png",
+            $post['slug'],
+            $post['hero_image'],
             $post['slug']
         );
     }
@@ -229,7 +247,7 @@ class Writer implements BlogInterface
      */
     public function getPostsByAuthor(int $authorId, array $options = []): array
     {
-        $options['author'] = [$authorId];
+        // $options['author'] = [$authorId];
         $useOptions = array_merge($this->options, $options);
         return $this->retrievePosts($useOptions);
     }
@@ -254,8 +272,10 @@ class Writer implements BlogInterface
     public function getCategories(): array
     {
         $categories = [];
-        $response = $this->client->request('GET', 'categories');
-        foreach ($response->toArray() as $category) {
+
+        $pdo = $this->db->getPdo();
+        $response = $pdo->query("SELECT * FROM categories")->fetchAll();
+        foreach ($response as $category) {
             $categories[] = new Category($category['id'], $category['name'], $category['slug']);
         }
         return $categories;
@@ -269,8 +289,9 @@ class Writer implements BlogInterface
     public function getTags(): array
     {
         $tags = [];
-        $response = $this->client->request('GET', 'tags');
-        foreach ($response->toArray() as $tag) {
+        $pdo = $this->db->getPdo();
+        $response = $pdo->query("SELECT * FROM tags")->fetchAll();
+        foreach ($response as $tag) {
             $tags[] = new Tag($tag['id'], $tag['name'], $tag['slug']);
         }
         return $tags;
